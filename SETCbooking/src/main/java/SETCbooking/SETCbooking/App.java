1a4b0c589a0e6f822ae3ac6f9f6eee3a8b059ff0
Sathish <Sathish@Sathish-PC>
2020-01-19 11:13:50 +0530

    initial co,mmit

---
+++ SETCbooking/src/main/java/SETCbooking/SETCbooking/App.java
@@ -0,0 +1,13 @@
+package SETCbooking.SETCbooking;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
